0c4e253e6f8160b1228d52a86485659ae4ca6dac
Kosmosama <luismg0408@gmail.com>
2025-03-11 19:32:47 +0100

    🧪Test: Add @smogon/calc library and try its calculations.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { calculate, Generations, Move, Pokemon } from '@smogon/calc';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styles: [],
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styles: [],
 })
 export class AppComponent {
-  title = 'frontend';
+    title = 'frontend';
+
+    test() {
+        const gen = Generations.get(5);
+        const move = new Move(gen, 'Close Combat');
+
+        const result = calculate(
+            gen,
+            new Pokemon(gen, 'Machamp', {
+                item: 'Choice Specs',
+                nature: 'Timid',
+                evs: { spa: 252 },
+                boosts: { spa: 1 },
+            }),
+            new Pokemon(gen, 'Eevee', {
+                item: 'Eviolite',
+                nature: 'Calm',
+                evs: { hp: 252, spd: 252 },
+            }),
+            move
+        );
+
+        console.log(result);
+    }
 }
