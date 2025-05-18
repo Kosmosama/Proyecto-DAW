fbd09af26c7c49632765e830f03972255a55ab5a
Kosmosama <luismg0408@gmail.com>
2025-05-18 23:46:16 +0200

    ⚡ Refactor: Game gateway should work now.

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -1,117 +1,131 @@
-import { WebSocketGateway, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect, MessageBody, ConnectedSocket } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { WebSocketGateway, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect, MessageBody, ConnectedSocket, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
 import { GameService } from './game.service';
 import { AuthService } from 'src/auth/auth.service';
 
 @WebSocketGateway({ namespace: 'game' })
-export class GameGateway{ // implements OnGatewayConnection, OnGatewayDisconnect 
-    // private logger = new Logger('GameGateway');
+export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
+    private logger = new Logger('GameGateway');
 
-    // constructor(
-    //     private readonly gameService: GameService,
-    //     private readonly authService: AuthService,
-    // ) { }
+    @WebSocketServer()
+    server: Server;
 
-    // /**
-    //  * Handles a new client connection.
-    //  * @param {Socket} client The socket client representing the connection.
-    //  * @returns {Promise<void>} No return value.
-    //  */
-    // async handleConnection(client: Socket) {
-    //     try {
-    //         const token = this.authService.extractToken(client);
-    //         const player = await this.authService.validateAccessToken(token);
-    //         client.data.player = player;
-    //         this.logger.log(`Client connected: ${player.username}#${player.tag}`);
-    //     } catch (err) {
-    //         this.logger.warn(`Unauthorized client tried to connect: ${client.id} ${err.message}`);
-    //         client.disconnect();
-    //     }
+    constructor(
+        private readonly gameService: GameService,
+        private readonly authService: AuthService,
+    ) { }
+
+    /**
+     * Handles a new client connection.
+     * @param {Socket} client The socket client representing the connection.
+     * @returns {Promise<void>} No return value.
+     */
+    async handleConnection(client: Socket) {
+        try {
+            const token = this.authService.extractToken(client);
+            const player = await this.authService.validateAccessToken(token);
+            client.data.player = player;
+            this.logger.log(`Client connected: ${player.username}#${player.tag}`);
+        } catch (err) {
+            this.logger.warn(`Unauthorized client tried to connect: ${client.id} ${err.message}`);
+            client.disconnect();
+        }
+    }
+
+    /**
+     * Handles client disconnection.
+     * @param {Socket} client The socket client representing the connection.
+     * @returns {Promise<void>} No return value.
+     */
+    async handleDisconnect(client: Socket) {
+        await this.gameService.removeFromQueue(client);
+        this.logger.log(`Client disconnected: ${client.id}`);
+    }
+
+    /**
+     * Handles a request to join a matchmaking queue.
+     * @param {Socket} client The socket client representing the connection.
+     * @returns {Promise<void>} No return value.
+     */
+    @SubscribeMessage('join-matchmaking')
+    async handleJoinMatchmaking(@ConnectedSocket() client: Socket) {
+        const player = client.data.player;
+        const room = await this.gameService.matchPlayer(this.server, client, player);
+        if (room) {
+            this.logger.log(`Match started in room: ${room}`);
+        }
+    }
+
+    /**
+     * Handles a request to leave the matchmaking queue.
+     * @param {Socket} client The socket client representing the connection.
+     * @returns {Promise<void>} No return value.
+     */
+    @SubscribeMessage('leave-matchmaking')
+    async handleLeaveMatchmaking(@ConnectedSocket() client: Socket) {
+        await this.gameService.removeFromQueue(client);
+    }
+
+    /**
+     * Handles a request to challenge a friend.
+     * @param {Socket} client The socket client representing the connection.
+     * @param {number} targetId The ID of the target player.
+     * @returns {Promise<void>} No return value.
+     */
+    @SubscribeMessage('challenge-friend')
+    async handleChallengeFriend(
+        @MessageBody() data: { targetId: number },
+        @ConnectedSocket() client: Socket,
+    ) {
+        const challenger = client.data.player;
+        const room = await this.gameService.challengeFriend(this.server, client, data.targetId, challenger);
+        if (room) {
+            this.logger.log(`Challenge started in room: ${room}`);
+        }
+    }
+
+    /**
+     * Handles a request to join a room as a spectator.
+     * @param {Socket} client The socket client representing the connection.
+     * @param {string} room The room ID to join.
+     * @returns {Promise<void>} No return value.
+     */
+    @SubscribeMessage('join-as-spectator')
+    handleJoinAsSpectator(
+        @MessageBody() data: { room: string },
+        @ConnectedSocket() client: Socket,
+    ) {
+        this.gameService.addSpectator(client, data.room);
+        this.logger.log(`Spectator joined room: ${data.room}`);
+    }
+
+    // #TODO Improve later
+    //      if player leaves room -> lose game
+    //      if spectator leaves room -> just leave
+    //      Check if room exists and if player/spectator is in it
+    // @SubscribeMessage('leave-room')
+    // async handleLeaveRoom(
+    //     @MessageBody() data: { roomId: string },
+    //     @ConnectedSocket() client: Socket,
+    // ) {
+    //     client.leave(data.roomId);
+    //     client.emit('left-room', { roomId: data.roomId });
     // }
 
-    // /**
-    //  * Handles client disconnection.
-    //  * @param {Socket} client The socket client representing the connection.
-    //  * @returns {Promise<void>} No return value.
-    //  */
-    // async handleDisconnect(client: Socket) {
-    //     await this.gameService.removeFromQueue(client);
-    //     this.logger.log(`Client disconnected: ${client.id}`);
-    // }
-
-    // /**
-    //  * Handles a request to join a matchmaking queue.
-    //  * @param {Socket} client The socket client representing the connection.
-    //  * @returns {Promise<void>} No return value.
-    //  */
-    // @SubscribeMessage('join-matchmaking')
-    // async handleJoinMatchmaking(@ConnectedSocket() client: Socket) {
+    /**
+     * Handles a request to leave a room.
+     * @param {Socket} client The socket client representing the connection.
+     * @param {string} room The room ID to leave.
+     * @returns {Promise<void>} No return value.
+     */
+    // @SubscribeMessage('chat-message')
+    // handleChatMessage(
+    //     @MessageBody() data: { room: string; message: string },
+    //     @ConnectedSocket() client: Socket,
+    // ) {
+    //     #TODO Should this have auth too? So that I dont have to pass data.player
     //     const player = client.data.player;
-    //     const room = await this.gameService.matchPlayer(client, player);
-    //     if (room) {
-    //         this.logger.log(`Match started in room: ${room}`);
-    //     }
+    //     this.gameService.sendChatMessage(client, data.room, player.username, data.message);
     // }
-
-    // /**
-    //  * Handles a request to leave the matchmaking queue.
-    //  * @param {Socket} client The socket client representing the connection.
-    //  * @returns {Promise<void>} No return value.
-    //  */
-    // @SubscribeMessage('leave-matchmaking')
-    // async handleLeaveMatchmaking(@ConnectedSocket() client: Socket) {
-    //     await this.gameService.removeFromQueue(client);
-    // }
-
-    // /**
-    //  * Handles a request to challenge a friend.
-    //  * @param {Socket} client The socket client representing the connection.
-    //  * @param {number} targetId The ID of the target player.
-    //  * @returns {Promise<void>} No return value.
-    //  */
-    // @SubscribeMessage('challenge-friend')
-    // async handleChallengeFriend(
-    //     @MessageBody() data: { targetId: number },
-    //     @ConnectedSocket() client: Socket,
-    // ) {
-    //     const challenger = client.data.player;
-    //     const room = await this.gameService.challengeFriend(client, data.targetId, challenger);
-    //     if (room) {
-    //         this.logger.log(`Challenge started in room: ${room}`);
-    //     }
-    // }
-
-    // /**
-    //  * Handles a request to join a room as a spectator.
-    //  * @param {Socket} client The socket client representing the connection.
-    //  * @param {string} room The room ID to join.
-    //  * @returns {Promise<void>} No return value.
-    //  */
-    // @SubscribeMessage('join-as-spectator')
-    // handleJoinAsSpectator(
-    //     @MessageBody() data: { room: string },
-    //     @ConnectedSocket() client: Socket,
-    // ) {
-    //     this.gameService.addSpectator(client, data.room);
-    //     this.logger.log(`Spectator joined room: ${data.room}`);
-    // }
-
-    // /**
-    //  * Handles a request to leave a room.
-    //  * @param {Socket} client The socket client representing the connection.
-    //  * @param {string} room The room ID to leave.
-    //  * @returns {Promise<void>} No return value.
-    //  */
-    // // @SubscribeMessage('chat-message')
-    // // handleChatMessage(
-    // //     @MessageBody() data: { room: string; message: string },
-    // //     @ConnectedSocket() client: Socket,
-    // // ) {
-    // //     #TODO Should this have auth too? So that I dont have to pass data.player
-    // //     const player = client.data.player;
-    // //     this.gameService.sendChatMessage(client, data.room, player.username, data.message);
-    // // }
-
-    // // #TODO Leave room when spectator/player
 }
--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -6,128 +6,133 @@ import { GameRoom } from './interfaces/game-room.interface';
 import Redis from 'ioredis';
 import { InjectRedis } from '@nestjs-modules/ioredis';
 import { GAME_ROOMS_PREFIX, MATCHMAKING_QUEUE, PLAYER_FRIENDS_PREFIX, PLAYER_SOCKETS_PREFIX } from 'src/config/redis.constants';
+import { Server } from 'socket.io';
 
 @Injectable()
 export class GameService {
-    // constructor(
-    //     private readonly playerService: PlayerService,
-    //     @InjectRedis() private readonly redis: Redis
-    // ) { }
+    constructor(
+        private readonly playerService: PlayerService,
+        @InjectRedis() private readonly redis: Redis
+    ) { }
 
-    // async matchPlayer(socket: Socket, player: PlayerPrivate): Promise<string | null> {
-    //     const opponentJson = await this.redis.lpop(MATCHMAKING_QUEUE);
-    //     if (opponentJson) {
-    //         const opponent = JSON.parse(opponentJson) as { id: number; socketId: string; username: string };
+    async matchPlayer(server: Server, socket: Socket, player: PlayerPrivate): Promise<string | null> {
+        const opponentJson = await this.redis.lpop(MATCHMAKING_QUEUE);
+        if (opponentJson) {
+            const opponent = JSON.parse(opponentJson) as { id: number; socketId: string; username: string };
 
-    //         const roomId = `match-${player.id}-${opponent.id}-${Date.now()}`;
-    //         await this.saveGameRoom(roomId, socket.id, opponent.socketId);
+            const [id1, id2] = [player.id, opponent.id].sort();
+            const roomId = `match-${id1}-${id2}-${Date.now()}`;
+            await this.saveGameRoom(roomId, socket.id, opponent.socketId);
 
-    //         socket.join(roomId);
-    //         const opponentSocket = await this.findSocket(opponent.socketId);
-    //         opponentSocket?.join(roomId);
+            socket.join(roomId);
+            const opponentSocket = this.findSocket(server, opponent.socketId);
+            opponentSocket?.join(roomId);
 
-    //         socket.emit('match-found', { roomId, opponent: opponent.username });
-    //         opponentSocket?.emit('match-found', { roomId, opponent: player.username });
-    //         return roomId;
-    //     }
+            socket.emit('match-found', { roomId, opponent: opponent.username });
+            opponentSocket?.emit('match-found', { roomId, opponent: player.username });
+            return roomId;
+        }
 
-    //     // Push self to queue
-    //     await this.redis.rpush(MATCHMAKING_QUEUE, JSON.stringify({
-    //         id: player.id,
-    //         socketId: socket.id,
-    //         username: player.username,
-    //     }));
+        await this.redis.rpush(MATCHMAKING_QUEUE, JSON.stringify({
+            id: player.id,
+            socketId: socket.id,
+            username: player.username,
+        }));
 
-    //     socket.emit('waitingForMatch');
-    //     return null;
-    // }
+        socket.emit('waiting-for-match');
+        return null;
+    }
 
-    // async challengeFriend(socket: Socket, targetId: number, challenger: PlayerPrivate): Promise<string | null> {
-    //     const isFriend = await this.areFriends(challenger.id, targetId);
-    //     if (!isFriend) {
-    //         socket.emit('error', 'Target user is not your friend');
-    //         return null;
-    //     }
+    async challengeFriend(server: Server, socket: Socket, targetId: number, challenger: PlayerPrivate): Promise<string | null> {
+        const isFriend = await this.areFriends(challenger.id, targetId);
+        if (!isFriend) {
+            socket.emit('error', 'Target user is not your friend');
+            return null;
+        }
 
-    //     const targetSocketId = await this.getFirstSocketId(targetId);
-    //     if (!targetSocketId) {
-    //         socket.emit('error', 'Target user is not online');
-    //         return null;
-    //     }
+        const targetSocketId = await this.getFirstSocketId(targetId);
+        if (!targetSocketId) {
+            socket.emit('error', 'Target user is not online');
+            return null;
+        }
 
-    //     const roomId = `challenge-${challenger.id}-${targetId}-${Date.now()}`;
-    //     await this.saveGameRoom(roomId, socket.id, targetSocketId);
+        const roomId = `challenge-${challenger.id}-${targetId}-${Date.now()}`;
+        await this.saveGameRoom(roomId, socket.id, targetSocketId);
 
-    //     socket.join(roomId);
-    //     const targetSocket = await this.findSocket(targetSocketId);
-    //     targetSocket?.join(roomId);
+        socket.join(roomId);
+        const targetSocket = this.findSocket(server, targetSocketId);
+        targetSocket?.join(roomId);
 
-    //     socket.emit('match-found', { roomId, opponentId: targetId });
-    //     targetSocket?.emit('match-found', { roomId, opponentId: challenger.id });
+        socket.emit('match-found', { roomId, opponentId: targetId });
+        targetSocket?.emit('match-found', { roomId, opponentId: challenger.id });
 
-    //     return roomId;
-    // }
+        return roomId;
+    }
 
-    // async removeFromQueue(socket: Socket) {
-    //     const entries = await this.redis.lrange(MATCHMAKING_QUEUE, 0, -1);
-    //     const updated = entries.filter((entry) => {
-    //         const parsed = JSON.parse(entry);
-    //         return parsed.socketId !== socket.id;
-    //     });
+    async removeFromQueue(socket: Socket) {
+        const entries = await this.redis.lrange(MATCHMAKING_QUEUE, 0, -1);
+        const updated = entries.filter((entry) => {
+            const parsed = JSON.parse(entry);
+            return parsed.socketId !== socket.id;
+        });
 
-    //     await this.redis.del(MATCHMAKING_QUEUE);
-    //     if (updated.length > 0) {
-    //         await this.redis.rpush(MATCHMAKING_QUEUE, ...updated);
-    //     }
-    // }
+        await this.redis.del(MATCHMAKING_QUEUE);
+        if (updated.length > 0) {
+            await this.redis.rpush(MATCHMAKING_QUEUE, ...updated);
+        }
+    }
 
-    // async addSpectator(socket: Socket, roomId: string) {
-    //     const gameRoomRaw = await this.redis.hget(`${GAME_ROOMS_PREFIX}active`, roomId);
-    //     if (!gameRoomRaw) {
-    //         socket.emit('error', 'Room not found');
-    //         return;
-    //     }
+    async addSpectator(socket: Socket, roomId: string) {
+        const gameRoomRaw = await this.redis.hget(`${GAME_ROOMS_PREFIX}active`, roomId);
+        if (!gameRoomRaw) {
+            socket.emit('error', 'Room not found');
+            return;
+        }
 
-    //     socket.join(roomId);
-    //     socket.emit('joined-as-spectator', { roomId });
-    // }
+        socket.join(roomId);
+        socket.emit('joined-as-spectator', { roomId });
+    }
 
-    // /**
-    //  * Sends a chat message to a specific game room.
-    //  * @param {Socket} socket The socket instance of the player sending the message.
-    //  * @param {string} roomId The ID of the game room.
-    //  * @param {string} username The username of the player sending the message.
-    //  * @param {string} message The chat message content.
-    //  */
-    // // #TODO Change later for persistance (??)
-    // sendChatMessage(socket: Socket, roomId: string, username: string, message: string) {
-    //     socket.to(roomId).emit('chat-message', { from: username, message });
-    // }
+    /**
+     * Sends a chat message to a specific game room.
+     * @param {Socket} socket The socket instance of the player sending the message.
+     * @param {string} roomId The ID of the game room.
+     * @param {string} username The username of the player sending the message.
+     * @param {string} message The chat message content.
+     */
+    // #TODO Change later for persistance (??)
+    sendChatMessage(socket: Socket, roomId: string, username: string, message: string) {
+        socket.to(roomId).emit('chat-message', { from: username, message });
+    }
 
-    // private async saveGameRoom(roomId: string, socketId1: string, socketId2: string) {
-    //     const roomData = {
-    //         roomId,
-    //         player1: socketId1,
-    //         player2: socketId2,
-    //         spectators: [],
-    //     };
+    private findSocket(server: Server, socketId: string): Socket | null {
+        return server.sockets.sockets.get(socketId) ?? null;
+    }
 
-    //     await this.redis.hset(`${GAME_ROOMS_PREFIX}active`, roomId, JSON.stringify(roomData));
-    // }
+    private async saveGameRoom(roomId: string, socketId1: string, socketId2: string) {
+        const roomData = {
+            roomId,
+            player1: socketId1,
+            player2: socketId2,
+            spectators: [],
+        };
 
-    // private async getFirstSocketId(playerId: number): Promise<string | null> {
-    //     const sockets = await this.redis.smembers(`${PLAYER_SOCKETS_PREFIX}${playerId}`);
-    //     return sockets[0] ?? null;
-    // }
+        await this.redis.hset(`${GAME_ROOMS_PREFIX}active`, roomId, JSON.stringify(roomData));
+    }
 
-    // private async areFriends(playerId: number, targetId: number): Promise<boolean> {
-    //     const redisKey = `${PLAYER_FRIENDS_PREFIX}${playerId}`;
-    //     const isFriend = await this.redis.sismember(redisKey, targetId.toString());
-    //     if (isFriend) return true;
+    private async getFirstSocketId(playerId: number): Promise<string | null> {
+        const sockets = await this.redis.smembers(`${PLAYER_SOCKETS_PREFIX}${playerId}`);
+        return sockets[0] ?? null;
+    }
 
-    //     // Fallback to DB
-    //     // #TODO Create function
-    //     // return this.playerService.areFriends(playerId, targetId);
-    //     return false; // Remove this line :D
-    // }
+    private async areFriends(playerId: number, targetId: number): Promise<boolean> {
+        const redisKey = `${PLAYER_FRIENDS_PREFIX}${playerId}`;
+        const isFriend = await this.redis.sismember(redisKey, targetId.toString());
+        if (isFriend) return true;
+
+        // Fallback to DB
+        // #TODO Create function
+        // return this.playerService.areFriends(playerId, targetId);
+        return false; // Remove this line :D
+    }
 }
