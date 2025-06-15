518ce0f477bd5fa644ecd9f75684efc3c3b5141c
Marcos <m.cires03@gmail.com>
2025-06-15 13:00:06 +0200

    🛠️ Fix: Oopsie

--- frontend/src/app/core/interceptors/base-url.interceptor.ts
+++ frontend/src/app/core/interceptors/base-url.interceptor.ts
@@ -1,7 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
-  const serverUrl = 'http://localhost:3000';
+  const serverUrl = environment.apiUrl;
   const reqClone = req.clone({
     url: `${serverUrl}/${req.url}`,
   });
